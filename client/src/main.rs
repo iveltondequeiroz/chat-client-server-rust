7718dfba273e60269b8e468b6f1d2618f5530b59
iveltondequeirozm <iveltondequeiroz@gmail.com>
2018-11-29 17:30:26 -0400

    client

--- client/src/main.rs
+++ client/src/main.rs
@@ -4,6 +4,11 @@ use std::sync::mspc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
 
+const LOCAL_HOST: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut client = TcpStream::connect(LOCAL_HOST).expect("Stream failed to connect");
+    client.set_nonblocking(true).expect("failed to initialize non-blocking");
 }
