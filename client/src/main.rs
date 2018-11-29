e9d2706471e2d38187cb07c8266b505c9722fbe0
iveltondequeirozm <iveltondequeiroz@gmail.com>
2018-11-29 17:27:13 -0400

    client

---
+++ client/src/main.rs
@@ -0,0 +1,9 @@
+use std::io::{self, ErrorKind, Read, Write};
+use std::net::TcpStream;
+use std::sync::mspc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    println!("Hello, world!");
+}
