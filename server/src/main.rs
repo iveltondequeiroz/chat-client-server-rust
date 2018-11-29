786a9c828660aa6f5518e63128e4359f179d5bad
iveltondequeirozm <iveltondequeiroz@gmail.com>
2018-11-29 11:58:36 -0400

    consts

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,6 +2,9 @@ use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::mpsc;
 
+const LOCAL_HOST: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
 fn main() {
     println!("Chat Server");
 }
